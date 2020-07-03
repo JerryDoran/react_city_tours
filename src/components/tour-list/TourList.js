1b652944ea5395248e9ecc661cb676a4e5066a17
Jerome Doran <disneyfan9184@gmail.com>
2020-07-03 16:24:40 -0400

    Finished project

--- src/components/tour-list/TourList.js
+++ src/components/tour-list/TourList.js
@@ -7,12 +7,21 @@ class TourList extends Component {
   state = {
     tours: tourData,
   };
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const filteredTours = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: filteredTours,
+    });
+  };
   render() {
     const { tours } = this.state;
     return (
       <section className='tour-list'>
         {tours.map((tour) => {
-          return <Tour key={tour.id} tour={tour} />;
+          return (
+            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+          );
         })}
       </section>
     );
