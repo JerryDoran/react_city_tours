fab869b9e62a0d4ccee8240278cbd6d620c366cb
Jerome Doran <disneyfan9184@gmail.com>
2020-07-14 18:07:45 -0400

    updated project

---
+++ build/precache-manifest.bf3a4dce137084a7b655804efcfce0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c72aad38af3dd757a1326b87a79620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8abf19133ceead619f35",
+    "url": "/static/css/main.642cca84.chunk.css"
+  },
+  {
+    "revision": "a38b62c8981502d61006",
+    "url": "/static/js/2.b7605bd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7605bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abf19133ceead619f35",
+    "url": "/static/js/main.5ec25eb1.chunk.js"
+  },
+  {
+    "revision": "a07e7d30bc64da84a9c0",
+    "url": "/static/js/runtime-main.914641fc.js"
+  }
+]);
